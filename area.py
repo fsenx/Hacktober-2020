b9c831d5ed86f5572d229c9fe70d87699daa0edc
Felipe <64341375+fsenx@users.noreply.github.com>
2020-10-01 22:32:40 -0300

    Add files via upload
    A simple example using function for to calculate the area  of object.

---
+++ area.py
@@ -0,0 +1,10 @@
+print("Hello, Now, let's to learn how to calculate the area of a simple object!")
+def area(a, b):
+    p = a * b
+    print(f"The sides are: {}m and {}m")
+    print(f"The area of object is {p}m²")
+
+
+a = float(input("Value of Width: "))
+b = float(input("Value of Lenght: "))
+área(a, b)
